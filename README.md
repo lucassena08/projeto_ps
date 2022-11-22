24d578f35770800fb513cf4beae6085dafd039bd
Rafael Ledo <Lucassena0810@gmail.com>
2022-11-22 00:16:54 -0300

    add views and update readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # projeto_ps
 projeto final de processos de software e programação front-end
+
+### Como usar
+
+<pre>
+Clonar o projeto
+
+<code>npm i</code>
+
+<code>nodemon app.js</code>
+
+</pre>
--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const app = express()
 
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html")
 })
+
+app.get("/:foo", (req, res) => {
+  const foo = req.params.foo
+
+  res.sendFile(__dirname + "/views/" + foo + ".html")
+})
+
 
 app.listen(3000, () => {
   console.log("Server stated on port 3000.")
