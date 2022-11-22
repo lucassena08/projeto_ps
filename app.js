7a839e30eb1d5deb970272f18eab159442f9bd8b
Rafael Ledo <Lucassena0810@gmail.com>
2022-11-21 23:58:16 -0300

    add server files, reorganize project folders

---
+++ app.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+})
+
+app.listen(3000, () => {
+  console.log("Server stated on port 3000.")
+})
