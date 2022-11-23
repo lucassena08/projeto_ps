f016b910b79a4da5578be9500d955b1fd2ec0564
Rafael Ledo <Lucassena0810@gmail.com>
2022-11-23 14:22:12 -0300

    implement loading-img

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -2,11 +2,16 @@ const formulario = document.querySelector('.input-sequence')
 const selecao = formulario.getElementsByTagName('select')
 const inputs = formulario.getElementsByTagName('input')
 const iterableInputs = [...inputs]
+const botaoForm = formulario.getElementsByTagName('button')[0]
+console.log(formulario)
+const imgFormLoad = formulario.getElementsByClassName('loading-img')[0]
 
-formulario.onsubmit = () => {
+formulario.onsubmit = e => {
+  e.preventDefault()
+
   localStorage.clear()
   const selectedIndex = selecao[0].options.selectedIndex
-  
+
   const selectedIndexTitle = selecao[0].options[selectedIndex].title
   const selectedIndexText = selecao[0].options[selectedIndex].innerText
   localStorage.setItem(selectedIndexTitle, selectedIndexText)
@@ -17,4 +22,12 @@ formulario.onsubmit = () => {
 
     localStorage.setItem(name, text)
   })
+
+  botaoForm.style.display = 'none'
+  imgFormLoad.style.display = 'block'
+
+  setInterval(() => {
+    imgFormLoad.style.display = 'none'
+    window.location.href += 'informacoes-investimento'
+  }, 1000)
 }
